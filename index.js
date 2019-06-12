9d9b60205366f53ed7ad0997234ca50455925974
kaiserawu <kaiser.a.wu@gmail.com>
2019-06-11 19:20:10 -0700

    Implement login with jwt generation

--- index.js
+++ index.js
@@ -25,6 +25,39 @@ server.post('/api/register', async (req, res) => {
   }
 })
 
+server.post('/api/login', async (req, res) => {
+  try {
+    const user = req.body;
+    console.log('user', user);
+  
+    const dbUserData = await db.select().from('users').where({ username: user.username }).first();
+    console.log('dbUserData', dbUserData);
+
+    if (dbUserData && await bcrypt.compare(user.password, dbUserData.password)) {
+      const token = generateToken(dbUserData);
+  
+      res.json({ message: `Welcome, here's a token!`, token: token });
+    } else {
+      res.status(401).json({ message: 'You shall not pass!' });
+    }
+  } catch(err) {
+    res.status(500).json({ error: err });
+  }
+})
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+
+  const options = {
+    expiresIn: '1d',
+  };
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
+
 const port = 4040;
 
 server.listen(port, () => {
