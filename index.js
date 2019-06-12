5a83c39bdce0e7e01d398224038097ae3263de18
kaiserawu <kaiser.a.wu@gmail.com>
2019-06-11 19:01:51 -0700

    Implement register endpoint

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ const server = express();
 
 server.use(express.json());
 
+server.post('/api/register', async (req, res) => {
+  try {
+    const user = req.body;
+    user.password = await bcrypt.hash(user.password, 14);
+    const returnedIdArr = await db.insert(user).into('users');
+    const dbEntry = await db.select().from('users').where({ id: returnedIdArr[0] }).first();
+    console.log(dbEntry);
+
+    res.status(201).json(dbEntry);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+})
+
 const port = 4040;
 
 server.listen(port, () => {
